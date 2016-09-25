a210bc4e6c087933c1ff0f4de53f785e0dcb06e2
Jesse Yates <jesse@fineo.io>
2016-09-24 17:06:15 -0700

    Adding help support to CreateOrg

--- model-testing/src/main/java/io/fineo/e2e/external/schema/CreateOrg.java
+++ model-testing/src/main/java/io/fineo/e2e/external/schema/CreateOrg.java
@@ -1,6 +1,8 @@
 package io.fineo.e2e.external.schema;
 
 import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParametersDelegate;
 import io.fineo.client.FineoClientBuilder;
 import io.fineo.client.model.schema.internal.CreateOrgRequest;
 import io.fineo.client.model.schema.internal.InternalSchemaApi;
@@ -14,12 +16,28 @@ public class CreateOrg {
 
   private static final Logger LOG = LoggerFactory.getLogger(CreateOrg.class);
 
-  public static void main(String[] args) throws Exception {
-    ApiOption api = new ApiOption();
-    OrgOption org = new OrgOption();
-    JCommander jc = new JCommander(new Object[]{api, org});
-    jc.parse(args);
+  @ParametersDelegate
+  private ApiOption api = new ApiOption();
 
+  @ParametersDelegate
+  private OrgOption org = new OrgOption();
+
+  @Parameter(names = {"-h", "--help"}, help = true)
+  private boolean help = false;
+
+  public static void main(String[] args) throws Exception {
+    CreateOrg create = new CreateOrg();
+    JCommander jc = new JCommander(new Object[]{create});
+    jc.parse(args);
+    if (create.help) {
+      jc.usage();
+      System.exit(0);
+    }
+
+    create.run();
+  }
+
+  private void run() throws Exception {
     LOG.debug("Using api-key [{}]", api.key);
     try (InternalSchemaApi schema = new FineoClientBuilder()
       .withEndpoint(api.url)
